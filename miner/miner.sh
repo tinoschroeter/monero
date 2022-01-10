42e6ac723cb953a4f0010499c463b84fca9d6d57
tinoschroeter <mail@wgnetworks.de>
2022-01-10 18:57:41 +0100

    update miner pool address

--- miner/miner.sh
+++ miner/miner.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 touch metrics.log
-
-./cpuminer -a cryptonight -o stratum+tcp://pool.minergate.com:45700 -u tino.schroeter@gmail.com \
+./cpuminer -a cryptonight -o stratum+tcp://xmc.pool.minergate.com:45560 -u tino.schroeter@gmail.com \
 | tee -a /var/log/metrics.log
